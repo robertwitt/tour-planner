5a5ddbce1532a973452ffccc6955cdc6641cff7c
robertwitt <robert_witt@icloud.com>
2022-06-30 15:03:51 +0200

    Implement service

--- srv/admin-service.js
+++ srv/admin-service.js
@@ -1,8 +1,11 @@
+const { DateTime } = require("luxon");
 const cds = require("@sap/cds");
+
+const MAX_END_DATE = "9999-12-31";
 
 class AdminService extends cds.ApplicationService {
   async init() {
-    const { Customers } = this.entities;
+    const { Customers, Workers } = this.entities;
 
     /**
      * Set the mandatory `isNaturalPerson` flag to `false` before creating a new customer (by activating the draft).
@@ -11,6 +14,29 @@ class AdminService extends cds.ApplicationService {
     this.before("CREATE", Customers, (req) => {
       if (req.data.isNaturalPerson === null) {
         req.data.isNaturalPerson = false;
+      }
+    });
+
+    /**
+     * Set defaults for `startDate` and `endDate` of a new worker.
+     */
+    this.before("CREATE", Workers, (req) => {
+      const data = req.data;
+      data.startDate =
+        data.startDate || data.endDate || DateTime.now().toISODate();
+      data.endDate = data.endDate || MAX_END_DATE;
+    });
+
+    /**
+     * Validate a worker's `startDate` and `endDate` before a draft is activated.
+     */
+    this.before("SAVE", Workers, (req) => {
+      const { startDate, endDate } = req.data;
+      if (!startDate || !endDate) {
+        return req.reject(400, "Start date and end date are mandatory");
+      }
+      if (!this._isValidDateRange(startDate, endDate)) {
+        return req.reject(400, "Start date must be lower or equal to end date");
       }
     });
 
@@ -33,6 +59,10 @@ class AdminService extends cds.ApplicationService {
     });
 
     await super.init();
+  }
+
+  _isValidDateRange(startDate, endDate) {
+    return DateTime.fromISO(startDate) <= DateTime.fromISO(endDate);
   }
 }
 
